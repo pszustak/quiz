9b2cffc6ccb9ef53285d84a6181f94ce489c0c46
Piotr Szustak <ppszustak@gmail.com>
2023-07-06 18:02:19 +0200

    Project initialize

---
+++ src/test/java/com/piotrszustak/quizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrszustak.quizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
