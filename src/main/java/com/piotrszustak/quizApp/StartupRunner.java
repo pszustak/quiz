9cf40668ac6189bdf49f11ca7f46bfc48f3b8b4b
Piotr Szustak <ppszustak@gmail.com>
2023-08-18 12:49:14 +0200

    refactor: CategoryService and getAllCategories renamed

--- src/main/java/com/piotrszustak/quizApp/StartupRunner.java
+++ src/main/java/com/piotrszustak/quizApp/StartupRunner.java
@@ -1,20 +1,20 @@
 package com.piotrszustak.quizApp;
 
-import com.piotrszustak.quizApp.services.QuizDataService;
+import com.piotrszustak.quizApp.services.CategoryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartupRunner implements CommandLineRunner {
 
-    private final QuizDataService quizDataService;
+    private final CategoryService categoryService;
 
-    public StartupRunner(final QuizDataService quizDataService) {
-        this.quizDataService = quizDataService;
+    public StartupRunner(final CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        quizDataService.getQuizCategories();
+        categoryService.getAllCategories();
     }
 }
--- src/main/java/com/piotrszustak/quizApp/services/CategoryService.java
+++ src/main/java/com/piotrszustak/quizApp/services/CategoryService.java
@@ -7,9 +7,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 @Log
-public class QuizDataService {
+public class CategoryService {
 
-    public CategoriesDto getQuizCategories() {
+    public CategoriesDto getAllCategories() {
         RestTemplate restTemplate = new RestTemplate();
         CategoriesDto result = restTemplate.getForObject("https://opentdb.com/api_category.php", CategoriesDto.class);
         log.info("Quiz categories: " + result.getCategories());
