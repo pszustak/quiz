4373beb54d1a628ccd98c8b0fa785548b0cd73a3
Piotr Szustak <ppszustak@gmail.com>
2023-08-22 18:10:33 +0200

    feature: categories to frontend

--- src/main/java/com/piotrszustak/quizApp/controllers/HomeController.java
+++ src/main/java/com/piotrszustak/quizApp/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.piotrszustak.quizApp.controllers;
 
+import com.piotrszustak.quizApp.services.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final CategoryService categoryService;
+
+    public HomeController(final CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/")
-    public String getHome() {
+    public String getHome(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "index";
     }
 }
