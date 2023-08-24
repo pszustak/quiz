d89e002f169ae4fcdf8635e6d1f76a66eed03a0c
Piotr Szustak <ppszustak@gmail.com>
2023-08-24 12:42:32 +0200

    refactor: index - number of questions

--- src/main/java/com/piotrszustak/quizApp/controllers/HomeController.java
+++ src/main/java/com/piotrszustak/quizApp/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.piotrszustak.quizApp.controllers;
 
 import com.piotrszustak.quizApp.services.CategoryService;
+import com.piotrszustak.quizApp.services.NumberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +11,17 @@ public class HomeController {
 
     private final CategoryService categoryService;
 
-    public HomeController(final CategoryService categoryService) {
+    private final NumberService numberService;
+
+    public HomeController(final CategoryService categoryService, final NumberService numberService) {
         this.categoryService = categoryService;
+        this.numberService = numberService;
     }
 
     @GetMapping("/")
     public String getHome(Model model) {
         model.addAttribute("categories", categoryService.getAllCategories());
+        model.addAttribute("numbers", numberService.getNumbers());
         return "index";
     }
 }
