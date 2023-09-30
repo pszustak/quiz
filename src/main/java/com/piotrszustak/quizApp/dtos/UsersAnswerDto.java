3ef9a38c2f4bb34a0d7cf67bc6a3b1ad2903c78a
Piotr Szustak <ppszustak@gmail.com>
2023-09-30 02:48:03 +0200

    feature: user's answer to backend

---
+++ src/main/java/com/piotrszustak/quizApp/dtos/UsersAnswerDto.java
@@ -0,0 +1,15 @@
+package com.piotrszustak.quizApp.dtos;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UsersAnswerDto {
+
+    private String answer;
+}
